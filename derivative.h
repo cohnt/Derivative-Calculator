de206fac32853dd466a0a2c669cf115ce1ae9abb
Thomas Cohn <cohnt@umich.edu>
2018-04-18 15:17:46 -0400

    Included the wrong file.

--- derivative.h
+++ derivative.h
@@ -5,8 +5,7 @@
 #include <vector>
 #include <algorithm>
 
-#include "polish_notation_converter.h"
-//Uses some of the helper functions
+#include "math_definitions.h"
 
 std::vector<Token> differentiate(std::vector<Token> function) {
 	//
