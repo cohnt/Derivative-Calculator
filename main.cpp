6e05993e551f820501fd37f25e66eb0d52f0c929
Thomas Cohn <cohnt@umich.edu>
2018-04-19 21:17:43 -0400

    Do as many equations as you want, or quit.

--- main.cpp
+++ main.cpp
@@ -18,60 +18,66 @@ int main(int argc, char** argv) {
 
 	PolishNotationConverter converter;
 	std::string input;
-	std::cout << "Equation to differentiate: ";
-	std::cin >> input;
-	std::cout << std::endl;
-	std::vector<Token> infix = converter.ParseString(input);
-	if(debug) {
-		std::cout << "Tokenized:" << std::endl;
-		for(int i=0; i<int(infix.size()); ++i) {
-			std::cout << infix[i] << " ";
+
+	while(true) {
+		std::cout << "Equation to differentiate, or q to quit: ";
+		std::cin >> input;
+		if(input == "q") {
+			break;
 		}
-		std::cout << std::endl << std::endl;
-	}
-
-	std::vector<Token> prefix = converter.InfixToPrefix(infix);
-	if(debug) {
-		std::cout << "Prefix:" << std::endl;
-		for(int i=0; i<int(prefix.size()); ++i) {
-			std::cout << prefix[i] << " ";
+		std::cout << std::endl;
+		std::vector<Token> infix = converter.ParseString(input);
+		if(debug) {
+			std::cout << "Tokenized:" << std::endl;
+			for(int i=0; i<int(infix.size()); ++i) {
+				std::cout << infix[i] << " ";
+			}
+			std::cout << std::endl << std::endl;
 		}
-		std::cout << std::endl << std::endl;
-	}
 
-	std::vector<Token> derivative = differentiate(prefix);
-	if(debug) {
-		std::cout << "Derivative:" << std::endl;
-		for(int i=0; i<int(derivative.size()); ++i) {
-			std::cout << derivative[i] << " ";
+		std::vector<Token> prefix = converter.InfixToPrefix(infix);
+		if(debug) {
+			std::cout << "Prefix:" << std::endl;
+			for(int i=0; i<int(prefix.size()); ++i) {
+				std::cout << prefix[i] << " ";
+			}
+			std::cout << std::endl << std::endl;
 		}
-		std::cout << std::endl << std::endl;
-	}
 
-	std::vector<Token> infix2 = converter.PrefixToInfix(derivative, 0, true);
-	if(debug) {
-		std::cout << "Infix:" << std::endl;
-		for(int i=0; i<int(infix2.size()); ++i) {
-			std::cout << infix2[i] << " ";
+		std::vector<Token> derivative = differentiate(prefix);
+		if(debug) {
+			std::cout << "Derivative:" << std::endl;
+			for(int i=0; i<int(derivative.size()); ++i) {
+				std::cout << derivative[i] << " ";
+			}
+			std::cout << std::endl << std::endl;
 		}
-		std::cout << std::endl << std::endl;
-	}
 
-	simplify(derivative, debug);
-
-	std::vector<Token> infix3 = converter.PrefixToInfix(derivative, 0, true);
-	if(debug) {
-		std::cout << "Simplified infix:" << std::endl;
-		for(int i=0; i<int(infix3.size()); ++i) {
-			std::cout << infix3[i] << " ";
+		std::vector<Token> infix2 = converter.PrefixToInfix(derivative, 0, true);
+		if(debug) {
+			std::cout << "Infix:" << std::endl;
+			for(int i=0; i<int(infix2.size()); ++i) {
+				std::cout << infix2[i] << " ";
+			}
+			std::cout << std::endl << std::endl;
 		}
-		std::cout << std::endl << std::endl;
+
+		simplify(derivative, debug);
+
+		std::vector<Token> infix3 = converter.PrefixToInfix(derivative, 0, true);
+		if(debug) {
+			std::cout << "Simplified infix:" << std::endl;
+			for(int i=0; i<int(infix3.size()); ++i) {
+				std::cout << infix3[i] << " ";
+			}
+			std::cout << std::endl << std::endl;
+		}
+
+		std::string output = converter.OutputString(infix3);
+
+		std::cout << "Result:" << std::endl;
+		std::cout << output << std::endl << std::endl;
 	}
-
-	std::string output = converter.OutputString(infix3);
-
-	std::cout << "Result:" << std::endl;
-	std::cout << output << std::endl << std::endl;
 
 	return 0;
 }
